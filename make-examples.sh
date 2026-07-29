f23b5d9f328c9c3e43cd8af99e2c9d04041ac254
louis <loudevroye@gmail.com>
2026-07-29 16:22:54 +0200

    [ADD] other targets to script

--- make-examples.sh
+++ make-examples.sh
@@ -1,2 +1,4 @@
 BUILDROOT_TARGET=keystone-examples-dirclean make -j$(nproc);
+#BUILDROOT_TARGET=keystone-runtime-dirclean make -j$(nproc);
+#BUILDROOT_TARGET=host-keystone-sdk-dirclean make -j$(nproc);
 make -j$(nproc);
